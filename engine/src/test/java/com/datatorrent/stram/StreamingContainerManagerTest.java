3730122733899d1eb2e7f0a70f5f6dfc9a5a59e6
David Yan <david@datatorrent.com>
2016-05-04 15:14:27 -0700

    test

--- engine/src/test/java/com/datatorrent/stram/StreamingContainerManagerTest.java
+++ engine/src/test/java/com/datatorrent/stram/StreamingContainerManagerTest.java
@@ -923,6 +923,7 @@ public class StreamingContainerManagerTest
   public void testAppDataPush() throws Exception
   {
     System.out.println("##################### ENV: " + System.getenv());
+    System.out.println("##################### PROP: " + System.getProperty("travis"));
     if (StramTestSupport.isInTravis()) {
       // disable this test in travis because of an intermittent problem similar to this:
       // http://stackoverflow.com/questions/32172925/travis-ci-sporadic-timeouts-to-localhost
